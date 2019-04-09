6496163be47a5c0cd72f9ff05dfa7c53ea6b72ea
Stephane Moreau <st.moreau@outlook.com.gr>
2019-04-09 13:04:40 +0100

    Updates

---
+++ precache-manifest.45ccdce21b44a74f8ef92bf3398aaaa2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9afc9fb7dbea8d74f0df7eacf4e281e",
+    "url": "/corgi-cms/static/media/logo.b9afc9fb.svg"
+  },
+  {
+    "revision": "6d0fc7796671f5e09a57",
+    "url": "/corgi-cms/static/js/runtime~main.19e547fc.js"
+  },
+  {
+    "revision": "3a29b06d2c6f9f1fe356",
+    "url": "/corgi-cms/static/js/main.bd753797.chunk.js"
+  },
+  {
+    "revision": "35f88e4c99828a8075ff",
+    "url": "/corgi-cms/static/js/2.6e6a7b1c.chunk.js"
+  },
+  {
+    "revision": "3a29b06d2c6f9f1fe356",
+    "url": "/corgi-cms/static/css/main.3a239a88.chunk.css"
+  },
+  {
+    "revision": "35f88e4c99828a8075ff",
+    "url": "/corgi-cms/static/css/2.53ec8488.chunk.css"
+  },
+  {
+    "revision": "8ff784b0f8e1854fe6dcc92db3fd3e64",
+    "url": "/corgi-cms/index.html"
+  }
+];
